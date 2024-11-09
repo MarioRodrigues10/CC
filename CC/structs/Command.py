5be9235eb81c9d323d0cbfd34310c932fa810dba
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-11-09 15:44:28 +0000

    fix: Command

--- CC/structs/Command.py
+++ CC/structs/Command.py
@@ -20,7 +20,8 @@ class PingCommand:
 
 
 class IPerfCommand:
-    def __init__(self, targets, transport, bytes, jitter_alert, loss_alert, bandwidth_alert):
+    SCALE = 1_000_000
+    def __init__(self, targets: list, transport: str, bytes: float, jitter_alert: float, loss_alert: float, bandwidth_alert: float):
         self.targets = targets
         self.transport = transport
         self.bytes = bytes
@@ -35,56 +36,52 @@ class IPerfCommand:
         jitter_alert_bytes = int(self.jitter_alert * self.SCALE).to_bytes(8, 'big')
         loss_alert_bytes = int(self.loss_alert * self.SCALE).to_bytes(8, 'big')
         bandwidth_alert_bytes = int(self.bandwidth_alert * self.SCALE).to_bytes(8, 'big')
-        return b"".join([targets_bytes, transport_bytes, bytes_bytes, jitter_alert_bytes, loss_alert_bytes, bandwidth_alert_bytes])
+        return b"".join([bytes_bytes, jitter_alert_bytes, loss_alert_bytes, bandwidth_alert_bytes, targets_bytes, transport_bytes])
 
     def deserialize(cls, data: bytes):
-        targets, transport, bytes, jitter_alert, loss_alert, bandwidth_alert = data.split(b',')
-        
-        targets = targets.decode('utf-8')
-        transport = transport.decode('utf-8')
-        bytes = int.from_bytes(bytes, 'big')
-        jitter_alert = int.from_bytes(jitter_alert, 'big') / cls.SCALE
-        loss_alert = int.from_bytes(loss_alert, 'big') / cls.SCALE
-        bandwidth_alert = int.from_bytes(bandwidth_alert, 'big') / cls.SCALE
-        
+        bytes = int.from_bytes(data[:8], 'big')
+        jitter_alert = int.from_bytes(data[8:16], 'big') / cls.SCALE
+        loss_alert = int.from_bytes(data[16:24], 'big') / cls.SCALE
+        bandwidth_alert = int.from_bytes(data[24:32], 'big') / cls.SCALE
+        targets = data[32:40].decode('utf-8')
+        transport = data[40:].decode('utf-8')
+                
         return cls(targets=targets, transport=transport, bytes=bytes, jitter_alert=jitter_alert, loss_alert=loss_alert, bandwidth_alert=bandwidth_alert)
 
 class IPCommand:
-    def __init__(self, targets, alert_down):
+    def __init__(self, targets: list, alert_down: bool):
         self.targets = targets
         self.alert_down = alert_down
         
     def serialize(self) -> bytes:
         targets_bytes = self.targets.encode('utf-8')
         alert_down_bytes = self.alert_down.to_bytes(1, 'big')
-        return b','.join([targets_bytes, alert_down_bytes])
+        return b"".join([alert_down_bytes, targets_bytes])
     
-    def deserialize(cls, data: bytes):
-        targets, alert_down = data.split(b',')
-        
-        targets = targets.decode('utf-8')
-        alert_down = alert_down.decode('utf-8')
+    def deserialize(cls, data: bytes):        
+        targets = targets.decode(data[1:])
+        alert_down = alert_down.decode(data[:1])
         
         return cls(targets=targets, alert_down=alert_down)
 
 
 class SystemMonitorCommand:
-    def __init__(self, targets, cpu_alert, memory_alert):
+    SCALE = 1_000_000
+    def __init__(self, targets: list, cpu_alert: float, memory_alert: float):
         self.targets = targets
         self.cpu_alert = cpu_alert
         self.memory_alert = memory_alert
 
     def serialize(self) -> bytes:
         targets_bytes = self.targets.encode('utf-8')
-        cpu_alert_bytes = self.cpu_alert.to_bytes(8, 'big')
-        memory_alert_bytes = self.memory_alert.to_bytes(8, 'big')
-        return b','.join([targets_bytes, cpu_alert_bytes, memory_alert_bytes])
+        cpu_alert_bytes = int(self.cpu_alert * self.SCALE).to_bytes(8, 'big')   
+        memory_alert_bytes = int(self.memory_alert * self.SCALE).to_bytes(8, 'big')
+        return b"".join([cpu_alert_bytes, memory_alert_bytes, targets_bytes])
     
     def deserialize(cls, data: bytes):
-        targets, cpu_alert, memory_alert = data.split(b',')
-        
-        targets = targets.decode('utf-8')
-        cpu_alert = float.from_bytes(cpu_alert, 'big')
-        memory_alert = float.from_bytes(memory_alert, 'big')
+
+        cpu_alert = int.from_bytes(data[:8], 'big') / cls.SCALE
+        memory_alert = int.from_bytes(data[8:16], 'big') / cls.SCALE
+        targets = data[16:].decode('utf-8')        
         
         return cls(targets=targets, cpu_alert=cpu_alert, memory_alert=memory_alert)
