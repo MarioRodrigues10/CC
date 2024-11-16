f684865eeef8294a2c869bd8e1d67bf4dcd97fd0
Afonso-santos <afonso.ds33@gmail.com>
2024-11-16 20:56:46 +0000

    feat:json and parser

--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,15 @@
 import common
+from server.json_parser import parser
 
 def main() -> None:
     print(common.SERVER_MESSAGE)
+
+    data = parser.parse_json()
+
+    for device_id, tasks in data.items():
+        print(f"Device ID: {device_id}")
+        for task in tasks:
+            print(f"  Task ID: {task.task_id}, Frequency: {task.frequency}")
 
 if __name__ == '__main__':
     main()
