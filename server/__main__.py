ddc0f509ea09ce46fc1c34a59761e24bceec2ec1
Afonso Santos <113265283+Afonso-santos@users.noreply.github.com>
2024-11-19 16:36:11 +0000

    feat: Json and Parser (#5)
    * feat:json and parser
    
    * fix:lint
    
    * fix:lint
    
    * fix:server and resquests changes
    
    * remove files

--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,28 @@
+import sys
 import common
+from server.json_parser import Parser
 
-def main() -> None:
+
+def main(path: str) -> None:
+    """
+    Main function to process tasks and display them by device ID.
+    """
     print(common.SERVER_MESSAGE)
 
-if __name__ == '__main__':
-    main()
+    json_parser = Parser()
+
+    data = json_parser.parse_json(path)
+
+    if data is None:
+        print("Error parsing JSON file")
+        return None
+    return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python -m server <file_path>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    print(f"Processing tasks from file: {input_file}")
+    main(input_file)
