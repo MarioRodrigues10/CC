190067d58f48dd96c5b5f9f9c679da8bb6235a19
voidbert <humbertogilgomes@protonmail.com>
2024-11-20 13:03:13 +0000

    feat: export message structures

--- common/__init__.py
+++ common/__init__.py
@@ -3,3 +3,11 @@ from .structs.IPCommand import IPCommand
 from .structs.IPerfCommand import IPerfCommand, TransportProtocol
 from .structs.PingCommand import PingCommand
 from .structs.SystemMonitorCommand import SystemMonitorCommand
+
+from .structs.Message import Message
+from .structs.IPOutput import IPOutput
+from .structs.IPerfOutput import IPerfOutput
+from .structs.PingOutput import PingOutput
+from .structs.SystemMonitorOutput import SystemMonitorOutput
+from .structs.MessagePrepare import MessagePrepare
+from .structs.MessageRegister import MessageRegister
