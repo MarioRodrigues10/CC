7ce7640b29a4ff9ea406af7e8432f60279cf2c1b
voidbert <humbertogilgomes@protonmail.com>
2024-11-20 14:45:59 +0000

    fix: reorder command tests to match import order

--- common/structs/CommandTests.py
+++ common/structs/CommandTests.py
@@ -1,18 +1,15 @@
 from unittest import TestCase, main
 
 from .. import \
-    Command, PingCommand, SystemMonitorCommand, IPerfCommand, IPCommand, TransportProtocol
+    Command, IPCommand, IPerfCommand, TransportProtocol, PingCommand, SystemMonitorCommand
 
-class CommandTest(TestCase):
-    def test_ping(self) -> None:
-        initial_command = PingCommand(['localhost', '1.1.1.1'], 10, 100.0)
-        command_bytes = initial_command.serialize()
-        final_command = Command.deserialize(command_bytes)
+class CommandTests(TestCase):
+    # NOTE:
+    # All floating point tests must be conducted with non-repeating numbers, so that equality
+    # comparisons don't fail due to lack of precision in encoding.
 
-        self.assertEqual(initial_command, final_command)
-
-    def test_system_monitor(self) -> None:
-        initial_command = SystemMonitorCommand(50.0, 100.0)
+    def test_ip(self) -> None:
+        initial_command = IPCommand(['wlan0', 'eth0'], True)
         command_bytes = initial_command.serialize()
         final_command = Command.deserialize(command_bytes)
 
@@ -28,8 +25,15 @@ class CommandTest(TestCase):
 
         self.assertEqual(initial_command, final_command)
 
-    def test_ip(self) -> None:
-        initial_command = IPCommand(['wlan0', 'eth0'], True)
+    def test_ping(self) -> None:
+        initial_command = PingCommand(['localhost', '1.1.1.1'], 10, 100.0)
+        command_bytes = initial_command.serialize()
+        final_command = Command.deserialize(command_bytes)
+
+        self.assertEqual(initial_command, final_command)
+
+    def test_system_monitor(self) -> None:
+        initial_command = SystemMonitorCommand(50.0, 100.0)
         command_bytes = initial_command.serialize()
         final_command = Command.deserialize(command_bytes)
 
