4d424cdd2f506e1fea8ac0554884530439b4bb1e
voidbert <humbertogilgomes@protonmail.com>
2024-12-01 00:51:55 +0000

    chore: rewrite UDP dynamic RTT measurement

--- common/NetTaskConnection.py
+++ common/NetTaskConnection.py
@@ -6,6 +6,7 @@ from .structs.NetTaskDataSegmentBody import NetTaskDataSegmentBody
 from .structs.NetTaskAckSegmentBody import NetTaskAckSegmentBody
 
 INITIAL_TIMEOUT = 5 # seconds
+MINIMUM_TIMEOUT = 0.005 # seconds (needed to avoid busy waiting on localhost)
 
 class NetTaskConnection:
     def __init__(self, own_host_name: str):
@@ -20,7 +21,10 @@ class NetTaskConnection:
         self.__other_max_ack = 0
         self.__last_sent_data_segment_time = time.time()
 
-    def __handle_received_data_segment(self, sequence: int, message: bytes) \
+        self.__rtt_avg_estimate: Optional[float] = None
+        self.__rtt_stdev_estimate: Optional[float] = None
+
+    def __handle_received_data_segment(self, sequence: int, time: float, message: bytes) \
         -> Optional[NetTaskSegment]:
 
         if sequence >= self.__next_sequence_to_receive:
@@ -34,22 +38,32 @@ class NetTaskConnection:
                     break
 
         return NetTaskSegment(0,
+                              time,
                               self.__own_host_name,
                               NetTaskAckSegmentBody(self.__own_max_ack))
-
         return None
 
-    def __handle_received_ack_segment(self, ack: int) -> Optional[NetTaskSegment]:
-        print('ACK', ack)
-
+    def __handle_received_ack_segment(self, ack: int, seg_time: float) -> Optional[NetTaskSegment]:
         # Remove segments we know don't need to be retransmitted
         for sequence in list(self.__unacked_segments):
             if sequence <= ack:
                 del self.__unacked_segments[sequence]
 
+        # Update RTT estimate
+        current_time = time.time()
+        delta = current_time - seg_time
+        if self.__rtt_avg_estimate is None or self.__rtt_stdev_estimate is None:
+            self.__rtt_stdev_estimate = delta
+            self.__rtt_avg_estimate = delta
+        else:
+            self.__rtt_stdev_estimate = \
+                0.75 * self.__rtt_stdev_estimate + 0.25 * abs(delta - self.__rtt_avg_estimate)
+            self.__rtt_avg_estimate = 0.875 * self.__rtt_avg_estimate + 0.125 * delta
+
         # Retranmsit if needed
         if ack < self.__next_sequence_to_send - 1 and ack <= self.__other_max_ack:
-            __last_sent_data_segment_time = time.time()
+            self.__last_sent_data_segment_time = current_time
+            self.__unacked_segments[ack + 1].time = current_time
             return self.__unacked_segments[ack + 1]
         else:
             self.__other_max_ack = ack
@@ -58,24 +72,34 @@ class NetTaskConnection:
 
     def handle_received_segment(self, segment: NetTaskSegment) -> Optional[NetTaskSegment]:
         if isinstance(segment.body, NetTaskDataSegmentBody):
-            return self.__handle_received_data_segment(segment.sequence, segment.body.message)
+            return self.__handle_received_data_segment(segment.sequence,
+                                                       segment.time,
+                                                       segment.body.message)
         elif isinstance(segment.body, NetTaskAckSegmentBody):
-            return self.__handle_received_ack_segment(segment.body.ack)
+            return self.__handle_received_ack_segment(segment.body.ack, segment.time)
 
         return None
 
+    def __retransmission_time_limit(self) -> float:
+        if self.__rtt_avg_estimate is None or self.__rtt_stdev_estimate is None:
+            return INITIAL_TIMEOUT
+        else:
+            return max(self.__rtt_avg_estimate + 4 * self.__rtt_stdev_estimate, MINIMUM_TIMEOUT)
+
     def time_until_next_timeout(self) -> float:
         current_time = time.time()
-        return max(INITIAL_TIMEOUT - (current_time - self.__last_sent_data_segment_time), 0.0)
+        elapsed = current_time - self.__last_sent_data_segment_time
+        return max(self.__retransmission_time_limit() - elapsed, 0.0)
 
     def act_on_timeout(self) -> Optional[NetTaskSegment]:
         current_time = time.time()
-        if current_time - self.__last_sent_data_segment_time  >= INITIAL_TIMEOUT:
-            self.__last_sent_data_segment_time  = current_time
+        if current_time - self.__last_sent_data_segment_time >= self.__retransmission_time_limit():
+            self.__last_sent_data_segment_time = current_time
 
             if len(self.__unacked_segments) > 0:
-                retransmit_sequence = self.__other_max_ack + 1
-                return self.__unacked_segments[retransmit_sequence]
+                retransmit_segment = self.__unacked_segments[self.__other_max_ack + 1]
+                retransmit_segment.time = current_time
+                return retransmit_segment
         
         return None
 
@@ -88,12 +112,14 @@ class NetTaskConnection:
         return to_receive
 
     def encapsulate_for_sending(self, message: bytes) -> NetTaskSegment:
+        current_time = time.time()
         segment = NetTaskSegment(self.__next_sequence_to_send,
+                                 current_time,
                                  self.__own_host_name,
                                  NetTaskDataSegmentBody(message))
 
         self.__unacked_segments[segment.sequence] = segment
         self.__next_sequence_to_send += 1
-        self.__last_sent_data_segment_time = time.time()
+        self.__last_sent_data_segment_time = current_time
 
         return segment
--- common/structs/NetTaskSegment.py
+++ common/structs/NetTaskSegment.py
@@ -1,43 +1,48 @@
+import struct
 from typing import Any, Self
 
 from .Message import SerializationException
 from .NetTaskSegmentBody import NetTaskSegmentBody
 
 class NetTaskSegment:
-    def __init__(self, sequence: int, host: str, body: NetTaskSegmentBody):
+    def __init__(self, sequence: int, time: float, host: str, body: NetTaskSegmentBody):
         self.sequence = sequence
+        self.time = time
         self.host = host
         self.body = body
 
     def serialize(self) -> bytes:
         sequence_bytes = self.sequence.to_bytes(4, 'big')
+        time_bytes = struct.pack('>d', self.time)
         host_bytes = self.host.encode('utf-8') + b'\0'
         body_bytes = self.body.serialize()
 
-        return b''.join([sequence_bytes, host_bytes, body_bytes])
+        return b''.join([sequence_bytes, time_bytes, host_bytes, body_bytes])
 
     @classmethod
     def deserialize(cls, data: bytes) -> Self:
-        if len(data) <= 5:
+        if len(data) <= 13:
             raise SerializationException('Incomplete NetTaskSegment')
 
         try:
             sequence = int.from_bytes(data[:4], 'big')
+            time = struct.unpack('>d', data[4:12])[0]
 
-            host_end = data.index(b'\0', 4)
-            host = data[4:host_end].decode('utf-8')
+            host_end = data.index(b'\0', 12)
+            host = data[12:host_end].decode('utf-8')
 
             body = NetTaskSegmentBody.deserialize(data[host_end + 1:])
         except (UnicodeError, ValueError) as e:
             raise SerializationException() from e
 
-        return cls(sequence, host, body)
+        return cls(sequence, time, host, body)
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, NetTaskSegment):
             return \
                 self.sequence == other.sequence and \
                 self.host == other.host and \
+                self.time == other.time and \
                 self.body == other.body
 
         return False
@@ -46,4 +51,5 @@ class NetTaskSegment:
         return 'NetTaskSegment(' \
             f'sequence={self.sequence}, ' \
             f'host={self.host}, ' \
+            f'time={self.time}, ' \
             f'body={self.body})'
